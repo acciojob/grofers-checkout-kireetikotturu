d6dcda3682f085e48e7d43447e737c408ad8e750
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 07:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let total = 0
+  const prices = document.querySelectorAll(".price");
+  prices.forEach((item, index) =>{
+    total+=(parseInt(item.textContent))
+  })
+  const newRow = document.createElement("tr");
+  
+  const tableData1 = document.createElement("td");
+  const tableData2 = document.createElement("td");
+  tableData1.textContent = "total";
+  tableData2.textContent = total;
+
+  newRow.appendChild(tableData1)
+  newRow.appendChild(tableData2)
+
+  let table = document.querySelector("table");
+  table.appendChild(newRow)
   
 };
 
